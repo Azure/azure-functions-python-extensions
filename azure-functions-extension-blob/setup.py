35f35153dc8add12682cb2255c507335d898a4dd
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2024-03-15 10:48:10 -0700

    fix

--- azure-functions-extension-blob/setup.py
+++ azure-functions-extension-blob/setup.py
@@ -3,6 +3,19 @@
 
 from setuptools import setup, find_packages
 
+EXTRA_REQUIRES = {
+    'dev': [
+        'flake8~=4.0.1',
+        'flake8-logging-format',
+        'mypy',
+        'pytest',
+        'pytest-cov',
+        'requests==2.*',
+        'coverage',
+        "pytest-instafail"
+    ]
+}
+
 setup(
     name='azure-functions-extension-blob',
     version='1.0.0a1',
@@ -14,10 +27,18 @@ setup(
         'azure', 'tests', 'samples'
     ]),
     classifiers=[
-        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
     ],
+    license='MIT',
+    extras_require=EXTRA_REQUIRES,
     python_requires='>=3.9',
     install_requires=[
         'azure-functions-extension-base',
