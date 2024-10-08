d855bd7fc895a2b463298bc6b0e3316b537b0c33
hallvictoria <59299039+hallvictoria@users.noreply.github.com>
2024-10-08 15:59:26 -0500

    feat: supporting managed identity (#45)
    * Error if invalid connection
    
    * Error for all types
    
    * Validate during type init
    
    * Missed refs
    
    * Removed testing line
    
    * Managed identity for Blob Client
    
    * Secrets typo
    
    * Secrets typo
    
    * MI for CC and SSD
    
    * lint
    
    * reorder checks
    
    * remove duplicate test
    
    * refactor env var check, client return
    
    * fix tests
    
    * refactor client creation, safe dict lookup
    
    ---------
    
    Co-authored-by: Victoria Hall <victoria.hall@microsoft.com>

--- azurefunctions-extensions-bindings-blob/azurefunctions/extensions/bindings/blob/containerClient.py
+++ azurefunctions-extensions-bindings-blob/azurefunctions/extensions/bindings/blob/containerClient.py
@@ -4,15 +4,16 @@
 import json
 from typing import Union
 
-from azure.storage.blob import ContainerClient as ContainerClientSdk
+from azure.storage.blob import BlobServiceClient
 from azurefunctions.extensions.base import Datum, SdkType
-from .utils import validate_connection_string
+from .utils import get_connection_string, using_managed_identity
 
 
 class ContainerClient(SdkType):
     def __init__(self, *, data: Union[bytes, Datum]) -> None:
         # model_binding_data properties
         self._data = data
+        self._using_managed_identity = False
         self._version = ""
         self._source = ""
         self._content_type = ""
@@ -24,15 +25,23 @@ class ContainerClient(SdkType):
             self._source = data.source
             self._content_type = data.content_type
             content_json = json.loads(data.content)
-            self._connection = validate_connection_string(content_json["Connection"])
-            self._containerName = content_json["ContainerName"]
-            self._blobName = content_json["BlobName"]
+            self._connection = get_connection_string(content_json.get("Connection"))
+            self._using_managed_identity = using_managed_identity(
+                content_json.get("Connection")
+            )
+            self._containerName = content_json.get("ContainerName")
+            self._blobName = content_json.get("BlobName")
 
     # Returns a ContainerClient
     def get_sdk_type(self):
         if self._data:
-            return ContainerClientSdk.from_connection_string(
-                conn_str=self._connection, container_name=self._containerName
+            blob_service_client = (
+                BlobServiceClient(account_url=self._connection)
+                if self._using_managed_identity
+                else BlobServiceClient.from_connection_string(self._connection)
+            )
+            return blob_service_client.get_container_client(
+                container=self._containerName
             )
         else:
             return None
