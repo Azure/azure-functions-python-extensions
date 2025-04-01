5a7c4b28a5d9be29187b9275aca0376139ca31bd
Evan Roman <evanroman@microsoft.com>
2025-04-01 01:20:43 -0500

    Rm 39

--- azurefunctions-extensions-bindings-eventhub/tests/test_eventdata.py
+++ azurefunctions-extensions-bindings-eventhub/tests/test_eventdata.py
@@ -46,18 +46,15 @@ class TestEventData(unittest.TestCase):
     def test_input_type(self):
         check_input_type = EventDataConverter.check_input_type_annotation
         self.assertTrue(check_input_type(EventData))
-        self.assertTrue(check_input_type(List[EventData]))
         self.assertFalse(check_input_type(str))
         self.assertFalse(check_input_type("hello"))
         self.assertFalse(check_input_type(bytes))
         self.assertFalse(check_input_type(bytearray))
-
-        # Generic types are not subscriptable in Python <3.9
-        if sys.version_info >= (3, 9):
-            self.assertTrue(check_input_type(list[EventData]))
-            self.assertTrue(check_input_type(tuple[EventData]))
-            self.assertTrue(check_input_type(set[EventData]))
-            self.assertFalse(check_input_type(dict[str, EventData]))
+        self.assertTrue(check_input_type(List[EventData]))
+        self.assertTrue(check_input_type(list[EventData]))
+        self.assertTrue(check_input_type(tuple[EventData]))
+        self.assertTrue(check_input_type(set[EventData]))
+        self.assertFalse(check_input_type(dict[str, EventData]))
 
     def test_input_none(self):
         result = EventDataConverter.decode(
