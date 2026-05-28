d93d07f19b82b240148823312f1cad5fa3e74d89
Victoria Hall <victoriahall@microsoft.com>
2026-05-28 14:16:04 -0500

    update versioning

--- azurefunctions-extensions-connectors/azurefunctions/extensions/connectors/__init__.py
+++ azurefunctions-extensions-connectors/azurefunctions/extensions/connectors/__init__.py
@@ -1,1 +1,6 @@
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License.
+
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+__version__ = '1.0.0b2'
